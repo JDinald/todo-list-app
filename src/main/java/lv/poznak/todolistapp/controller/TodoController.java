9c5ab75d37a3396044e11668aa6f0f68946240f4
vladislavs.poznaks <vladislavs.poznaks@accenture.com>
2021-01-25 08:48:37 +0200

    *Test for tomcat

--- src/main/java/lv/poznak/todolistapp/controller/TodoController.java
+++ src/main/java/lv/poznak/todolistapp/controller/TodoController.java
@@ -1,13 +1,12 @@
 package lv.poznak.todolistapp.controller;
 
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 /*@RequestMapping("/api")*/
-@RequiredArgsConstructor
 public class TodoController {
 
   @GetMapping("/test")
